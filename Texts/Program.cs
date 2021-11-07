40edbcd7fd7bc070dbf016767b586ba2ab807832
Caitlin Bond <401307@vistacollege.nl>
2021-11-07 14:16:41 +0100

    Add project files.

---
+++ Texts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Texts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("enter firtst text");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("second firtst text");
+            string b = Console.ReadLine();
+
+            int c = a.Length + b.Length;
+
+            Console.WriteLine("LENGTH first text " + a.Length + "\n LENGTH Second text " + b.Length + "\n LENGTH text toegetor " + c);
+        }
+    }
+}
