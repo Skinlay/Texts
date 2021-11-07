c42e74b669458ffdbb62429f017ad70bf1529026
Caitlin Bond <401307@vistacollege.nl>
2021-11-07 14:26:56 +0100

    opdracht 2 toegevoed: en programma dat de eerste 2 letters van de voornaam en achternaam samenvoegt.

--- Texts/Program.cs
+++ Texts/Program.cs
@@ -8,6 +8,7 @@ namespace Texts
         {
             Console.WriteLine("Hello World!");
 
+            /*
             Console.WriteLine("enter firtst text");
             string a = Console.ReadLine();
 
@@ -17,6 +18,19 @@ namespace Texts
             int c = a.Length + b.Length;
 
             Console.WriteLine("LENGTH first text " + a.Length + "\n LENGTH Second text " + b.Length + "\n LENGTH text toegetor " + c);
+            */
+
+            Console.WriteLine("enter first name");
+
+            string firstName = Console.ReadLine();
+            string first = firstName.Substring(0, 2);
+
+            Console.WriteLine("enter first name");
+            string LastName = Console.ReadLine();
+            string Last = LastName.Substring(0, 2);
+
+            Console.WriteLine(first + Last);
+
         }
     }
 }
